e755feaab0c5d91262308a604864557ed71b58bb
Soumik Mukherjee <smsoumik1608@gmail.com>
2021-01-18 20:01:26 +0530

    Logical error rectified

--- spiralMatrix.cpp
+++ spiralMatrix.cpp
@@ -19,35 +19,46 @@ int main(){
     }
 
     int row_start = 0, col_start =0, row_end = n-1, col_end = m-1;
+    int direction = 0;
 
     while(row_start <= row_end && col_start <= col_end){
 
         // for starting row
-        for(int col = col_start; col <= col_end; col++){
+        if(direction == 0){
+           for(int col = col_start; col <= col_end; col++){
             cout<<a[row_start][col]<<" ";
+            }
+            row_start++;  
         }
-        row_start++;
+       
 
         // for ending column
-
-        for(int row = row_start; row <= row_end; row++){
+        if(direction == 1){
+           for(int row = row_start; row <= row_end; row++){
             cout<<a[row][col_end]<<" ";
+            }
+            col_end--; 
         }
-        col_end--;
+        
 
         // for ending row
-
-        for(int col = col_end; col>=col_start; col--){
+        if(direction == 2){
+           for(int col = col_end; col>=col_start; col--){
             cout<<a[row_end][col]<<" ";
+            }
+            row_end--;
+ 
         }
-        row_end--;
-
+        
         // for starting column
-
-        for(int row = row_end; row >= row_start; row--){
+        if(direction == 3){
+           for(int row = row_end; row >= row_start; row--){
             cout<<a[row][col_start]<<" ";
+            }
+            col_start++; 
         }
-        col_start++;
+        
+        direction = (direction+1)%4;
 
     }
 
