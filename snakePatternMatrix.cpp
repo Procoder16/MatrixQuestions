c2f491c0fa7ac093615d62ddc7eba48a0ed70980
Procoder16 <smsoumik1608@gmail.com>
2021-06-21 16:45:22 +0530

    printing matrix in snake pattern added

---
+++ snakePatternMatrix.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int m,n;
+    cin>>m>>n;
+
+    int arr[m][n];
+
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        if(i%2 == 0){
+            for(int j = 0; j < n; j++){
+                cout<<arr[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        else{
+            for(int j = n-1; j>=0; j--){
+                cout<<arr[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+
+    return 0;
+}
