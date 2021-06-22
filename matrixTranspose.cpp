42f96c2d544727ff87f8ba2dc388fb281a7d1c24
Procoder16 <smsoumik1608@gmail.com>
2021-06-22 11:12:59 +0530

    changed the code a little bit

--- matrixTranspose.cpp
+++ matrixTranspose.cpp
@@ -25,10 +25,8 @@ int main(){
     }
 
     for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            int temp = a[i][j];
-            a[i][j] = a[j][i];
-            a[j][i] = temp;
+        for(int j=0;j<i;j++){
+            swap(a[i][j], a[j][i]);
         }
     }
 
